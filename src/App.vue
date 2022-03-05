bddf1cfa3aa4aaf64bb30ded99f05046240d890f
ruan.pasta <ruan.pasta@senior.com.br>
2022-03-05 13:16:23 -0300

    first commmit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <BarraNavegacao />
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import BarraNavegacao from "./components/BarraNavegacao.vue";
+
+export default {
+  components: {
+    BarraNavegacao,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="jumbotron mt-2">
+      <h1 class="display-4">Bem-vindo!</h1>
+      <p>Aplição disponível para listagem dos gerentes do nosso banco.</p>
+      <button class="btn btn-danger" @click="efetuarLogout">Sair</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { logoutMixin } from "@/mixins";
+
+export default {
+  name: "home",
+  mixins: [logoutMixin],
+};
+</script>
